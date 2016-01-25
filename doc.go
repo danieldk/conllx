8dc1373c4943f50dbfb6c00f5da4672a5dc8101a
Daniël de Kok <me@danieldk.eu>
2016-01-25 09:36:50 +0100

    Add license.

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The conllx Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package conllx provides readers and a writer for the CoNNL-X format:
 //
 // http://ilk.uvt.nl/conll/
