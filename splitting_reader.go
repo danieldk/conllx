0367765b290cc24d08aa6145408d0d69d3252609
Daniël de Kok <me@danieldk.eu>
2016-01-25 10:36:45 +0100

    Add a splitting reader.
    Splitting readers can be used for e.g. cross-validation.

---
+++ splitting_reader.go
@@ -0,0 +1,59 @@
+// Copyright 2015 The conllx Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package conllx
+
+import (
+	"errors"
+	"io"
+)
+
+// A FoldSet contains fold numbers. This type is used with a SplittingReader
+// to indicate from which folds sentences should be returned.
+type FoldSet map[int]interface{}
+
+// SplittingReader is a wrapper around a (CoNLL-X) Reader that splits the
+// corpus into folds.
+type SplittingReader struct {
+	reader Reader
+	nFolds int
+	folds  FoldSet
+	count  int
+}
+
+// NewSplittingReader creates a SplittingReader, that splits the data in
+// 'nFolds' folds. The reader returns the sentences that are in 'folds'.
+func NewSplittingReader(reader Reader, nFolds int, folds FoldSet) (*SplittingReader, error) {
+	if nFolds < 1 {
+		return nil, errors.New("The data should be 'splitted' in at least 1 fold.")
+	}
+
+	return &SplittingReader{
+		reader: reader,
+		nFolds: nFolds,
+		folds:  folds,
+		count:  -1,
+	}, nil
+}
+
+// ReadSentence returns the next sentence that is in one of the folds
+// requested from the SplittingReader.
+func (r *SplittingReader) ReadSentence() (sentence []Token, err error) {
+	for {
+		sentence, err := r.reader.ReadSentence()
+		if err == io.EOF {
+			return sentence, err
+		}
+
+		r.count++
+
+		if r.count == r.nFolds {
+			r.count = 0
+		}
+
+		if _, ok := r.folds[r.count]; ok {
+			return sentence, nil
+		}
+	}
+}
---
+++ splitting_reader_test.go
@@ -0,0 +1,100 @@
+package conllx
+
+import (
+	"io"
+	"testing"
+)
+
+const splitTestFragment string = `1	a
+2	b
+
+1	c
+2	d
+
+1	e
+2	f
+
+1	g
+2	h
+
+1	i
+2	j`
+
+var splitTestSent0 = []Token{
+	*NewToken().SetForm("a"),
+	*NewToken().SetForm("b"),
+}
+
+var splitTestSent2 = []Token{
+	*NewToken().SetForm("e"),
+	*NewToken().SetForm("f"),
+}
+
+var splitTestSent3 = []Token{
+	*NewToken().SetForm("g"),
+	*NewToken().SetForm("h"),
+}
+
+var splitTestSent4 = []Token{
+	*NewToken().SetForm("i"),
+	*NewToken().SetForm("j"),
+}
+
+type splittingTestCase struct {
+	nFolds   int
+	folds    FoldSet
+	expected [][]Token
+}
+
+var splittingTestCases = []splittingTestCase{
+	splittingTestCase{
+		nFolds: 2,
+		folds:  map[int]interface{}{0: nil},
+		expected: [][]Token{
+			splitTestSent0,
+			splitTestSent2,
+			splitTestSent4,
+		},
+	},
+	splittingTestCase{
+		nFolds: 3,
+		folds:  map[int]interface{}{0: nil, 2: nil},
+		expected: [][]Token{
+			splitTestSent0,
+			splitTestSent2,
+			splitTestSent3,
+		},
+	},
+}
+
+func TestSplitting(t *testing.T) {
+	for _, testCase := range splittingTestCases {
+		r, err := NewSplittingReader(stringReader(splitTestFragment), testCase.nFolds, testCase.folds)
+		if err != nil {
+			t.Fatal("unexpected error:", err)
+		}
+
+		for _, correct := range testCase.expected {
+			sent, err := r.ReadSentence()
+			equalOrFail(t, err, correct, sent)
+		}
+
+		_, err = r.ReadSentence()
+		if err != io.EOF {
+			t.Fatalf("Reader should return EOF.")
+		}
+	}
+}
+
+func TestSplittingNoFolds(t *testing.T) {
+	folds := map[int]interface{}{}
+	r, err := NewSplittingReader(stringReader(splitTestFragment), 2, folds)
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+
+	_, err = r.ReadSentence()
+	if err != io.EOF {
+		t.Fatal("expected immediate EOF when reading no folds")
+	}
+}
