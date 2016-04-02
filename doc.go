13da2977df448811e6a96465ced982898aeddc43
Daniël de Kok <me@danieldk.eu>
2016-04-02 14:51:18 +0200

    Package documentation fixes.

--- doc.go
+++ doc.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package conllx provides readers and a writer for the CoNNL-X format:
+// Package conllx provides readers and a writer for the CoNLL-X format.
 //
+// More information about CONLL-X can be found at:
 // http://ilk.uvt.nl/conll/
 package conllx
