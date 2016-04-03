0d44ec81778febba319def5ecd06e1546000bf5c
Daniël de Kok <me@danieldk.eu>
2016-04-03 11:02:10 +0200

    Example for Token.(Set)?Features.

--- conll_test.go
+++ conll_test.go
@@ -1,6 +1,11 @@
 package conllx
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"strings"
+)
 
 func ExampleNewToken() {
 	// Construct a token using the builder-pattern
@@ -12,5 +17,44 @@ func ExampleNewToken() {
 
 	fmt.Println(sent)
 	// Output: 1	apples	apple	_	noun	_	_	_	_	_
+}
 
+func ExampleToken_Features() {
+	input := `1	test	_	_	_	f1:v1|f2:v2`
+	strReader := strings.NewReader(input)
+	reader := NewReader(bufio.NewReader(strReader))
+
+	sent, err := reader.ReadSentence()
+	if err != nil {
+		log.Fatal("Error reading sentence")
+	}
+
+	features, ok := sent[0].Features()
+	if !ok {
+		log.Fatal("Token should have features")
+	}
+
+	fmt.Println(features.FeaturesMap()["f1"])
+	fmt.Println(features.FeaturesMap()["f2"])
+
+	// Output:
+	// v1
+	// v2
+}
+
+func ExampleToken_SetFeatures() {
+
+	token := NewToken().SetFeatures(map[string]string{
+		"num":   "sg",
+		"tense": "past",
+	})
+
+	features, _ := token.Features()
+
+	fmt.Println(features.FeaturesMap()["num"])
+	fmt.Println(features.FeaturesMap()["tense"])
+
+	// Output:
+	// sg
+	// past
 }
